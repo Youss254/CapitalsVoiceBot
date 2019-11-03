6c32fa789ba39ac3db219610a9820f3f78391dbb
Youssef Chaabani <youssef-chaabani25@ieee.org>
2019-11-03 13:12:50 +0100

    third commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace capitals
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(50, 84);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(141, 239);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(412, 84);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(141, 239);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pays";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(412, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "capitale";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoSize = true;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(591, 343);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "capitals";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Globalization;
+using System.Threading;
+
+namespace capitals
+{
+    public partial class Form1 : Form
+    {
+            SpeechSynthesizer S = new SpeechSynthesizer();
+            Choices list = new Choices();
+            //public Boolean cap = false;
+            public Form1()
+        {
+            CultureInfo ci = new CultureInfo("fr-FR");
+            SpeechRecognitionEngine rec = new SpeechRecognitionEngine(ci);
+            list.Add(File.ReadAllLines(@"C:\Users\thinkpad\Desktop\cap.txt"));
+            Grammar gr = new Grammar(new GrammarBuilder(list));
+            try
+            {
+                rec.RequestRecognizerUpdate();
+                rec.LoadGrammarAsync(gr);
+                rec.SpeechRecognized += Rec_SpeechRecognized;
+                rec.SetInputToDefaultAudioDevice();
+                rec.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch { return; }
+            S.SelectVoiceByHints(VoiceGender.Female);
+            S.Volume = 100;
+            InitializeComponent();
+        }
+        public void say(string c)
+        {
+            S.SpeakAsync(c);
+            textBox2.AppendText(c + "\n");
+        }
+        private void Rec_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string res = e.Result.Text;
+            //if (cap)
+            //{
+                if (res.Equals(res.ToUpper()))
+                {
+                    int l = 0;
+                    StreamReader sr = new StreamReader(@"C:\Users\thinkpad\Desktop\cap.txt");
+                    List<string> lines = new List<string>();
+                    while (lines.Contains(res) == false)
+                    {
+                        lines.Add(sr.ReadLine());
+                    
+                        l++;
+
+                    }
+                    sr.Close();
+                    say(lines[l - 2]);
+                    label2.ForeColor = Color.Red;
+                    /*Thread.Sleep(1000);
+                    Process.Start("https://www.google.tn/search?q=" + lines[l - 2]);*/
+                    //cap = false;
+                }
+                else
+                {
+                    int l = 0;
+                    StreamReader sr = new StreamReader(@"C:\Users\thinkpad\Desktop\cap.txt");
+                    List<string> lines = new List<string>();
+                    while (lines.Contains(res) == false)
+                    {
+                        lines.Add(sr.ReadLine());
+                        l++;
+
+                    }
+                    lines.Add(sr.ReadLine());
+                    say(res + " est la capitale de " + lines[l]);
+                    sr.Close();
+                    //cap = false;
+                }
+            
+           //}
+           /*if (res == "capitale")
+           {
+               cap = true;
+           }*/
+           textBox1.AppendText(res + "\n");
+        }
+            private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
